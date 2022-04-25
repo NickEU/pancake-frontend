9d85818408b87eff836f53d150247a3835ddcb76
Nikolajs Korolkovs <nikolajs.korolkovs@protonmail.com>
2022-04-25 14:07:19 +0300

    fix: changed the RPC node helper to ftm

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -8,14 +8,8 @@ if (
 }
 
 // Array of available nodes to connect to
-export const nodes = [process.env.NEXT_PUBLIC_NODE_1, process.env.NEXT_PUBLIC_NODE_2, process.env.NEXT_PUBLIC_NODE_3]
-
+export const nodes = [process.env.PROPER_RPC_NODE]
 const getNodeUrl = () => {
-  // Use custom node if available (both for development and production)
-  // However on the testnet it wouldn't work, so if on testnet - comment out the NEXT_PUBLIC_NODE_PRODUCTION from env file
-  if (process.env.NEXT_PUBLIC_NODE_PRODUCTION) {
-    return process.env.NEXT_PUBLIC_NODE_PRODUCTION
-  }
   return sample(nodes)
 }
 
