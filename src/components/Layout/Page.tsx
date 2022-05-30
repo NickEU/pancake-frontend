a2ad4881410983464901b2a254049fa6d60a06da
Nikolajs Korolkovs <k@nick.tf>
2022-05-30 23:45:09 +0000

    chore: removed icon from title

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -26,12 +26,12 @@ export const PageMeta: React.FC<{ symbol?: string }> = ({ symbol }) => {
   const { t } = useTranslation()
   const { pathname } = useRouter()
   const cakePriceUsd = useCakeBusdPrice()
-  const cakePriceUsdDisplay = cakePriceUsd ? `$${cakePriceUsd.toFixed(3)}` : '...'
+  // const cakePriceUsdDisplay = cakePriceUsd ? `$${cakePriceUsd.toFixed(3)}` : '...'
 
   const pageMeta = getCustomMeta(pathname, t) || {}
   const { title, description, image } = { ...DEFAULT_META, ...pageMeta }
-  let pageTitle = cakePriceUsdDisplay ? [title, cakePriceUsdDisplay].join(' - ') : title
-  if (symbol) {
+  let pageTitle = title // cakePriceUsdDisplay ? [title, cakePriceUsdDisplay].join(' - ') : title
+  if (false) {
     pageTitle = [symbol, title].join(' - ')
   }
 
